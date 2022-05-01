42342de9feed20fe1c0bba2a6c3d55506aaa5afd
bestecetin <beste.cetinn@gmail.com>
2022-05-02 00:41:04 +0300

    Laravel Show Data Category

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -33,21 +33,6 @@
                     <div class="x_panel">
                         <div class="x_title">
                             <h2>Category List</h2>
-                            <ul class="nav navbar-right panel_toolbox">
-                                <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                                </li>
-                                <li class="dropdown">
-                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                                    <ul class="dropdown-menu" role="menu">
-                                        <li><a href="#">Settings 1</a>
-                                        </li>
-                                        <li><a href="#">Settings 2</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li><a class="close-link"><i class="fa fa-close"></i></a>
-                                </li>
-                            </ul>
                             <div class="clearfix"></div>
                         </div>
                         <div class="x_content">
@@ -76,7 +61,8 @@
                                         <td>{{$rs->image}}</td>
                                         <td>{{$rs->status}}</td>
                                         <td><a href="/admin/category/edit/{{$rs->id}}" class="btn btn-round btn-primary">Edit</a></td>
-                                        <td><a href="/admin/category/delete/{{$rs->id}}" class="btn btn-round btn-danger">Delete</a></td>
+                                        <td><a href="/admin/category/destroy/{{$rs->id}}" class="btn btn-round btn-danger"
+                                            onclick="return confirm('Deleting!!! Are you sure ?')">Delete</a></td>
                                         <td><a href="/admin/category/show/{{$rs->id}}" class="btn btn-round btn-success">Show</a></td>
                                     </tr>
                                 @endforeach
---
+++ resources/views/admin/category/show.blade.php
@@ -0,0 +1,79 @@
+
+@extends('layouts.adminbase')
+
+@section('title', 'Edit Category:'.$data->title)
+
+
+@section('content')
+
+    <!-- page content -->
+    <div class="right_col" role="main">
+        <div class="">
+            <div class="page-title">
+                <div class="title_left">
+                    <a href="/admin/category/edit/{{$data->id}}" class="btn btn-info" style="width: 200px">Edit</a>
+                    <a href="/admin/category/destroy/{{$data->id}}" onclick="return confirm('Deleting!!! Are you sure ?')" class="btn btn-danger" style="width: 200px">Delete</a>
+                </div>
+
+                <div class="title_right">
+                    <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
+                        <div class="input-group">
+                            <input type="text" class="form-control" placeholder="Search for...">
+                            <span class="input-group-btn">
+                      <button class="btn btn-default" type="button">Go!</button>
+                    </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="clearfix"></div>
+
+            <div class="row">
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                    <div class="x_panel">
+                        <div class="x_title">
+                            <h2>Detail</h2>
+                            <div class="clearfix"></div>
+                        </div>
+
+
+                            <table class="table table-striped">
+                                <tr>
+                                    <th style="width: 150px">Id</th>
+                                    <td>{{$data->id}}</td>
+                                </tr>
+                                <tr>
+                                    <th>Title</th>
+                                    <td>{{$data->title}}</td>
+                                </tr>
+                                <tr>
+                                    <th>Keywords</th>
+                                    <td>{{$data->keywords}}</td>
+                                </tr>
+                                <tr>
+                                    <th>Image</th>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <th>Status</th>
+                                    <td>{{$data->status}}</td>
+                                </tr>
+                                <tr>
+                                    <th>Created Date</th>
+                                    <td>{{$data->created_at}}</td>
+                                </tr>
+                                <tr>
+                                    <th>Updated Date</th>
+                                    <td>{{$data->updated_at}}</td>
+                                </tr>
+                            </table>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /page content -->
+
+@endsection
